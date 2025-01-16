866addc94af4895b7e7f1f630cc0b97130a1f388
大雨 <dayu@dayudeMacBook-Air.local>
2025-01-16 10:36:21 +0800

    feat: scp and frame connect polocies changed

--- nextjs/csp/policies/walletConnect.ts
+++ nextjs/csp/policies/walletConnect.ts
@@ -4,6 +4,7 @@ import config from 'configs/app';
 
 import { KEY_WORDS } from '../utils';
 
+
 export function walletConnect(): CspDev.DirectiveDescriptor {
   if (!config.features.blockchainInteraction.isEnabled) {
     return {};
@@ -15,10 +16,15 @@ export function walletConnect(): CspDev.DirectiveDescriptor {
       '*.walletconnect.com',
       'wss://relay.walletconnect.com',
       'wss://www.walletlink.org',
+      'https://api.web3modal.org',
+      'https://rpc.walletconnect.org',
     ],
     'frame-ancestors': [
       '*.walletconnect.org',
       '*.walletconnect.com',
+      'http://localhost:*',
+      'https://*.dbcscan.io',  // 添加测试环境域名
+      'https://test.dbcscan.io'  // 明确指定测试环境域名
     ],
     'img-src': [
       KEY_WORDS.BLOB,
