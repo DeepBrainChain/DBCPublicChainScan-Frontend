4c364e23614da9371d8ef8b14ea701a2f26600ce
qingye <2863960964@qq.com>
2025-03-26 21:53:39 +0800

    fix:json and logo

--- pages/mymachine/index.tsx
+++ pages/mymachine/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getEnvValue } from '../../configs/app/utils';
 
 import PageNextJs from 'nextjs/PageNextJs';
 const MyMachine = dynamic(() => import('ui/mymachine/index'), { ssr: false });
 console.log(process.env.NEXT_PUBLIC_API_URLX || '环境变量没生效'); // 应输出 "DeepBrainChain Testnet"
-console.log(getEnvValue('NEXT_PUBLIC_API_URLx'), 'getEnvValue', process.env);
+console.log(getEnvValue('NEXT_PUBLIC_API_URLX'), 'getEnvValue', process.env);
 
-console.log(process.env.NEXT_PUBLIC_API_SPEC_URL); // 应输出 "DeepBrainChain Testnet"
+useEffect(() => {
+  console.log(getEnvValue('NEXT_PUBLIC_API_URLX')); // 输出 "https://testnet.dbcscan.io/mymachine"
+}, []);
 
 const Page: NextPage = () => (
   <PageNextJs pathname="/mymachine">
