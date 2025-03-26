1a051d4d896c6cc7cfb95e086fc1e54f178ddb97
qingye <2863960964@qq.com>
2025-03-26 09:50:13 +0800

    fix:rpc node

--- lib/web3/wagmiConfig.ts
+++ lib/web3/wagmiConfig.ts
@@ -8,8 +8,7 @@ const feature = config.features.blockchainInteraction;
 // import { getEnvValue } from '../../configs/app/utils';
 
 const wagmiConfig = (() => {
-  // const rpcUrl = process.env.NEXT_PUBLIC_NETWORK_RPC_URL || 'https://rpc-testnet.dbcwallet.io';
-  const rpcUrl = process.env.NEXT_PUBLIC_NETWORK_RPC_URL || 'https://rpc.dbcwallet.io';
+  const rpcUrl = process.env.NEXT_PUBLIC_NETWORK_RPC_URL || 'https://rpc-testnet.dbcwallet.io';
   console.log('RPC URLAAAAAAAAAAAAAAAAAAAAA:', rpcUrl);
   try {
     if (!feature.isEnabled) {
@@ -22,7 +21,7 @@ const wagmiConfig = (() => {
       chains,
       multiInjectedProviderDiscovery: true,
       transports: {
-        [currentChain.id]: http('https://rpc.dbcwallet.io'),
+        [currentChain.id]: http(rpcUrl),
       },
       projectId: feature.walletConnect.projectId,
       metadata: {
