56b05df59988055e141f022b683b8fbed4df40a7
qingye <2863960964@qq.com>
2025-03-31 14:13:23 +0800

    fix:text and dbc unstake

--- next.config.js
+++ next.config.js
@@ -46,7 +46,7 @@ const moduleExports = {
         loader: 'ignore-loader', // 使用 ignore-loader
       });
     }
-    console.log('222xx11266xxcx22Xxxxxxxxxxx');
+    console.log('22');
     return config;
   },
   rewrites,
