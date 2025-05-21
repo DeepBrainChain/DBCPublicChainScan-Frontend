f173d21be313190b4217cfe03362b0091f904ff0
qingye <2863960964@qq.com>
2025-05-21 16:15:13 +0800

    fix:bug

--- lib/web3/currentChain.ts
+++ lib/web3/currentChain.ts
@@ -1,27 +1,54 @@
+// import { type Chain } from 'viem';
+
+// import config from 'configs/app';
+
+// const currentChain = {
+//   id: Number(config.chain.id),
+//   name: config.chain.name ?? '',
+//   nativeCurrency: {
+//     decimals: config.chain.currency.decimals,
+//     name: config.chain.currency.name ?? '',
+//     symbol: config.chain.currency.symbol ?? '',
+//   },
+//   rpcUrls: {
+//     'default': {
+//       http: [ config.chain.rpcUrl ?? '' ],
+//     },
+//   },
+//   blockExplorers: {
+//     'default': {
+//       name: 'Blockscout',
+//       url: config.app.baseUrl,
+//     },
+//   },
+//   testnet: config.chain.isTestnet,
+// } as const satisfies Chain;
+
+// export default currentChain;
+
 import { type Chain } from 'viem';
 
 import config from 'configs/app';
 
 const currentChain = {
-  id: Number(config.chain.id),
-  name: config.chain.name ?? '',
+  id: 19880818,
+  name: 'DeepBrainChain Mainnet',
   nativeCurrency: {
-    decimals: config.chain.currency.decimals,
-    name: config.chain.currency.name ?? '',
-    symbol: config.chain.currency.symbol ?? '',
+    decimals: 18,
+    name: 'DeepBrainChain',
+    symbol: 'DBC',
   },
   rpcUrls: {
-    'default': {
-      http: [ config.chain.rpcUrl ?? '' ],
+    default: {
+      http: ['https://rpc.dbcwallet.io'],
     },
   },
   blockExplorers: {
-    'default': {
+    default: {
       name: 'Blockscout',
-      url: config.app.baseUrl,
+      url: 'https://dbcscan.io',
     },
   },
-  testnet: config.chain.isTestnet,
+  testnet: false,
 } as const satisfies Chain;
-
 export default currentChain;
