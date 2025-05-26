a66f965b74a2ce7bc1a7636fe7240f7f81748a37
qingye <2863960964@qq.com>
2025-05-26 10:26:41 +0800

    fix:bug

--- lib/web3/currentChain.ts
+++ lib/web3/currentChain.ts
@@ -1,54 +1,27 @@
-// import { type Chain } from 'viem';
-
-// import config from 'configs/app';
-
-// const currentChain = {
-//   id: Number(config.chain.id),
-//   name: config.chain.name ?? '',
-//   nativeCurrency: {
-//     decimals: config.chain.currency.decimals,
-//     name: config.chain.currency.name ?? '',
-//     symbol: config.chain.currency.symbol ?? '',
-//   },
-//   rpcUrls: {
-//     'default': {
-//       http: [ config.chain.rpcUrl ?? '' ],
-//     },
-//   },
-//   blockExplorers: {
-//     'default': {
-//       name: 'Blockscout',
-//       url: config.app.baseUrl,
-//     },
-//   },
-//   testnet: config.chain.isTestnet,
-// } as const satisfies Chain;
-
-// export default currentChain;
-
 import { type Chain } from 'viem';
 
 import config from 'configs/app';
 
 const currentChain = {
-  id: 19880818,
-  name: 'DeepBrainChain Mainnet',
+  id: Number(config.chain.id),
+  name: config.chain.name ?? '',
   nativeCurrency: {
-    decimals: 18,
-    name: 'DeepBrainChain',
-    symbol: 'DBC',
+    decimals: config.chain.currency.decimals,
+    name: config.chain.currency.name ?? '',
+    symbol: config.chain.currency.symbol ?? '',
   },
   rpcUrls: {
     default: {
-      http: ['https://rpc.dbcwallet.io'],
+      http: [config.chain.rpcUrl ?? ''],
     },
   },
   blockExplorers: {
     default: {
       name: 'Blockscout',
-      url: 'https://dbcscan.io',
+      url: config.app.baseUrl,
     },
   },
-  testnet: false,
+  testnet: config.chain.isTestnet,
 } as const satisfies Chain;
+
 export default currentChain;
--- ui/gpumachine/modules/WithdrawBtn-btn-dialog.tsx
+++ ui/gpumachine/modules/WithdrawBtn-btn-dialog.tsx
@@ -130,7 +130,7 @@ function WithdrawBtn({ fetchMachineInfoData, id }: { fetchMachineInfoData: any; 
       const signer = await provider.getSigner();
 
       const contract = new ethers.Contract(
-        '0xc663ee691f98d80e5f1cce3c9ad53a14da676a23', // 替换为你真实的 CPU_CONTRACT_ADDRESS_STAKING
+        CPU_CONTRACT_ADDRESS_STAKING, // 替换为你真实的 CPU_CONTRACT_ADDRESS_STAKING
         stakingAbi,
         signer
       );
@@ -183,7 +183,7 @@ function WithdrawBtn({ fetchMachineInfoData, id }: { fetchMachineInfoData: any; 
   async function getRewardInfoH(address: string) {
     try {
       const balance = await readContract(config, {
-        address: '0xc663ee691f98d80e5f1cce3c9ad53a14da676a23',
+        address: CPU_CONTRACT_ADDRESS_STAKING,
         abi: stakingAbi,
         functionName: 'getRewardInfo',
         args: [address],
